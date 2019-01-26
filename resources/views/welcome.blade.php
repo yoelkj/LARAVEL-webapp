ba3cce6b69cc4e8de10a7ca72746a072a8cd85e9
Yoel Velasquez Valencia <yoel.velasquez.valencia@gmail.com>
2019-01-26 03:36:36 -0500

    Initial commit - hello world

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello world - LARAVEL</title>
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
+
+            <div class="content">
+                <h1>Yoel Velasquez Valencia</h1>
+                <p>Email: Yoel.velasquez.valencia@gmail.com</p>
+                <p>Skype: yoelkj</p>
+                <p>Website: velcodi.com</p>
+            </div>
+        </div>
+    </body>
+</html>
