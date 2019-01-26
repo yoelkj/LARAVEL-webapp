3f06e173c708ee86623d7cc2bcf54c1c54557672
Yoel Velasquez Valencia <yoel.velasquez.valencia@gmail.com>
2019-01-26 05:09:11 -0500

    Add bootstrap styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,22 @@
 
         <title>Hello world - LARAVEL</title>
 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <style>
+            .fa-ul{margin:0;}
+            .fa-ul li{text-align:center;}
+            .fa-ul li a{color:black!important;font-size:1.5em;}            
+            .fa-ul li .fa-stack{width:auto!important;color:black!important;}
+            .bg-primary{background-color:#00a6ec!important;}
+
+            @media (max-width: 425px) { 
+                .fa-ul li a{font-size:1.2em;}
+            }
+            @media (max-width: 375px) { 
+                .fa-ul li a{font-size:0.9em;}
+            }    
+        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -21,12 +37,45 @@
                 </div>
             @endif
 
-            <div class="content">
-                <h1>Yoel Velasquez Valencia</h1>
-                <p>Email: Yoel.velasquez.valencia@gmail.com</p>
-                <p>Skype: yoelkj</p>
-                <p>Website: velcodi.com</p>
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col col-md-8 col-lg-6">
+                        <div class="card text-white bg-primary mt-3">
+                        <div class="card-header text-center">
+                        <h1>Yoel Velasquez Valencia</h1>
+                        <h3>Freelance</h3>
+                        <h5>Fullstack</h5>
+                        </div>
+                        <ul class="list-group list-group-flush fa-ul">
+                            <li class="list-group-item"><span class="fa-stack fa-2x d-block" >
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fas fa-envelope fa-stack-1x fa-inverse"></i>
+                                </span>
+                                <a href="mailto:Yoel.velasquez.valencia@gmail.com">{ yoel.velasquez.valencia@gmail.com }</a></li>
+                            <li class="list-group-item"><span class="fa-stack fa-2x d-block" >
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-skype fa-stack-1x fa-inverse"></i>
+                                </span>
+                                <a href="javascript:void(0);">{ Yoelkj }</a></li>
+                            <li class="list-group-item"><span class="fa-stack fa-2x d-block" >
+                                    <i class="fas fa-circle fa-stack-2x"></i>
+                                    <i class="fas fa-link fa-stack-1x fa-inverse"></i>
+                                </span>
+                                <a href="http://velcodi.com">{ velcodi.com }</a></li>
+                        </ul>
+                        <!--
+                        <div class="card-body"></div>
+                        -->
+                        <div class="card-footer">
+                            <p class="text-right">Laravel-webapp</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     </body>
 </html>
